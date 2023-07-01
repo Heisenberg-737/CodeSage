9bb6edeb70f5a4ce9d545f73a747988f3f6ed941
Heisenberg-737 <pranay.kothari17@gmail.com>
2023-07-02 04:55:33 +0530

    package json updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,35 @@
+// @ts-check
+
 /**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
  */
-await import("./src/env.mjs");
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    domains: ["cdn.discordapp.com", "upload.wikimedia.org"],
 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "/a/**",
+      },
+    ],
+  },
   /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
+   * If you have the "experimental: { appDir: true }" setting enabled, then you
+   * must comment the below `i18n` config out.
    *
    * @see https://github.com/vercel/next.js/issues/41980
    */
@@ -19,5 +38,4 @@ const config = {
     defaultLocale: "en",
   },
 };
-
 export default config;
